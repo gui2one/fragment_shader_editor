365e15695096a3ae1691f1e658f7e76662529b60
gui2one <guillaume.rouault.fx@gmail.com>
2025-03-22 20:53:06 +0100

    installed magnificient GlslEditor ....

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import { FragmentCanvas } from "./components/canvas.ts";
+import GlslEditor from "glslEditor/build/glslEditor";
 console.log("main.ts loaded");
 
-let canvas = new FragmentCanvas();
-document.body.appendChild(canvas);
+let code_editor = document.createElement("div");
+
+document.body.appendChild(code_editor);
+
+const glslEditor = new GlslEditor(code_editor, {
+  canvas_size: 500,
+  canvas_draggable: false,
+  theme: "monokai",
+  multipleBuffers: false,
+  watchHash: true,
+  fileDrops: true,
+  menu: true,
+});
